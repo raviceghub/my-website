1c94bda759ae3565807754b3140735e96457b623
raviceghub <97589277+raviceghub@users.noreply.github.com>
2026-05-27 17:51:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-function addToCart(product, price) {
 
-    // your existing logic...
-
-    window.dataLayer = window.dataLayer || [];
-    dataLayer.push({
-        event: "add_to_cart",
-        product_name: product,
-        product_price: price
-    });
-}
